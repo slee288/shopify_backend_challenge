0c2428f2bbe07b90e027293ac187983ddede3c18
slee <home@slees-MacBook-Air.local>
2019-01-19 15:31:49 -0500

    Fix Errors

--- resolvers.js
+++ resolvers.js
@@ -2,7 +2,7 @@ var express = require("express");
 var router = express.Router();
 var mongoose = require("mongoose");
 
-var Product = require("./models/Product");
+var Product = require("./models/product");
 
 // Resolver collections to be used as functions in GraphQL
 const resolvers = {
